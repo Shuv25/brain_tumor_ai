4ccab5b1e7c437b1c2fa2a50fbce0d3bf025c616
Shuv@25 <shuvsutradhar@gmail.com>
2025-05-10 11:01:19 +0530

    added new functionality 'segemnation'

--- utils/image_processing.py
+++ utils/image_processing.py
@@ -1,16 +1,6 @@
 from PIL import Image
 import numpy as np
 
-# def process_image(img_path, target_size=(224, 224)):
-#     img = Image.open(img_path)
-#     img = img.resize(target_size)
-
-#     img_array = np.array(img)
-
-#     img_array = np.expand_dims(img_array, axis=0)  
-
-#     return img_array
-
 def process_image(img_path):
     img = Image.open(img_path).convert('RGB')
     img = img.resize((224, 224))
